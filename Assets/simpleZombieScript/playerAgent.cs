c456129eea5fca2ba1d86f240c2fabc4a8978219
Song Kyung <skdn159@naver.com>
2018-07-13 20:09:02 +0900

    scripting playerAgent, botenemy

---
+++ Assets/simpleZombieScript/playerAgent.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAgent : Agent
+{
+    RayPerception _rayPercept;
+    Rigidbody agentRB;
+
+    // need?
+    List<GameObject> nearZombieList = new List<GameObject>();
+
+
+
+    // ****** TODO ******
+    // heading Vector
+    // Type입력하면 바뀌어서 능력치 
+    // playerType 
+    // *******************
+
+
+    float _moveSpeed;
+
+    float _shotCoolTime;
+    int _hp;
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+
+        _rayPercept = GetComponent<RayPerception>();
+
+        //agentRB = GetComponent<Rigidbody>();
+        //shootAction = GetComponent<ShootAction>();
+        //_shootPower = generalInfo.shotPower;
+        //_dpsCoolTime = generalInfo.dpsCoolTime;
+        //_attackBonus = generalInfo.attackBonus;
+        //_turnSpeed = generalInfo.turnSpeed;
+        //_moveSpeed = generalInfo.moveSpeed;
+        //_curTeam = Teams.Team_agent;
+
+        ResetAgentValue();
+    }
+
+
+
+    public override void CollectObservations()
+    {
+
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+
+    }
+
+    public override void AgentReset()
+    {
+        ResetAgentValue();
+    }
+
+    public override void AgentOnDone()
+    {
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    void Shot()
+    {
+        // zombie에게 shot
+
+
+
+    }
+
+    void ResetAgentValue()
+    {
+        // curState
+        //_hp = generalInfo.maxHp;
+        //_curState = BotState.normal;
+        //_shootingTime = Time.time;
+        //
+        //totalAttackPoint = 0;
+
+        this.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/simpleZombieScript/simpleZombieAcademy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleZombieAcademy : Academy
+{
+    /*
+    [HideInInspector]
+    public GameObject[] BotAgentList;
+
+    [HideInInspector]
+    public GameObject[] BotEnemyList;
+    */
+
+
+    [HideInInspector]
+    public zombieGround[] groundList;
+
+    public int totalScore;
+    //public Text scoreText;
+
+    public static simpleZombieAcademy instance;
+
+    public void Start() // 
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public override void AcademyReset()
+    {
+        //groundList = FindObjectsOfType<zombieGround>();
+        //Debug.Log("IN  AcademyReset : BotAgentCount = " + BotAgentList.Length);
+
+        foreach (zombieGround zg in groundList)
+        {
+            // todo
+
+            //zg.ResetBattleArea(BotAgentList, BotEnemyList);
+
+        }
+
+        totalScore = 0;
+
+        ObjectManager.instance.FreeAllObj(ObjectType.missile);
+    }
+
+    public override void AcademyStep()
+    {
+        //Debug.Log("academy step called");
+
+        /*
+        scoreText.text = string.Format(@"Score: {0}", totalScore);
+        */
+    }
+}
