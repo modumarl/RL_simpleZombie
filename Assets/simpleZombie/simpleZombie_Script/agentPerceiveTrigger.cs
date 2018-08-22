c2110c2864d140b5948130abe2f0836d817513fd
SongK <skdn159@naver.com>
2018-08-23 02:46:13 +0900

    updating : add Range Trigger

---
+++ Assets/simpleZombie/simpleZombie_Script/agentPerceiveTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agentPerceiveTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "zombie")
+        {
+            Debug.LogWarning("ZOMBIE ENTER!@!@!@!@!@");
+
+            // todo 
+            // obj를 parent obj script에 넘기면
+            // instance ID 봐 가면서 list 관리 할것 
+            // Trigger EXit도 마찬가지 
+
+        }
+
+
+
+    }
+}
